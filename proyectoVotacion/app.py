d8165ac74786e17ff607634a66c8fbc09c8bb0ad
Juana Guerra <guerra.116127@e.unavarra.es>
2023-03-19 16:08:54 +0100

    [#7] - Implementar la lógica de la página web y crear el API REST

--- proyectoVotacion/app.py
+++ proyectoVotacion/app.py
@@ -1,24 +1,93 @@
-import time
-from flask import Flask
-import mysql.connector
-
-
+from flask import Flask, render_template, request,Response,jsonify
+from back import Database
 
 app = Flask(__name__)
 
+config = {
+    'user': 'root',
+    'password': 'root',
+    'host': 'mysql',
+    'db': 'voting_data',
+}
 
-connection = mysql.connector.connect(
-  user='root', password='root', host='mysql', port="3306", database='patata')
-print("DB connected")
+@app.route('/', methods=['GET','POST'])
+def voting():
+    try:
+      if request.method == 'POST':
+        db = Database(config)
+        if request.form.get('cat') == 'votar':
+          db.vote('cat')
+        elif request.form.get('dog') == 'votar':
+          db.vote('dog')
+        cats = db.getVotes('cat')
+        dogs = db.getVotes('dog')
+        db.close()
+        return render_template('voting.html', dog_votes= "Número de votos: " + str(dogs), cat_votes= "Número de votos: " + str(cats))
+      return render_template('voting.html', dog_votes= "", cat_votes= "")
+    except:
+      message = {
+        'message': 'Error internal'
+      }
+      resp = jsonify(message)
+      resp.status_code = 500
+      return resp
 
-cursor = connection.cursor()
-cursor.execute('Select * FROM users')
-students = cursor.fetchall()
-connection.close()
-
-#print(students)
-
-@app.route('/')
-def hello():
-    count = 1
-    return 'Hello World! I have patata been seen {}.{} patata\n'.format(count,students)
+@app.route('/api/v1/votation/cats', methods=['GET'])
+def getCats():
+  try:
+    db = Database(config)
+    cats = db.getVotes('cat')
+    db.close()
+    message = {
+       'cat_votes': cats
+    }
+    resp = jsonify(message)
+    resp.status_code = 200
+    return resp
+  except:
+    message = {
+       'message': 'Error internal'
+    }
+    resp = jsonify(message)
+    resp.status_code = 500
+    return resp
+  
+@app.route('/api/v1/votation/dogs', methods=['GET'])
+def getDogs():
+  try:
+    db = Database(config)
+    dogs = db.getVotes('dog')
+    db.close()
+    message = {
+       'dog_votes': dogs
+    }
+    resp = jsonify(message)
+    resp.status_code = 200
+    return resp
+  except:
+    message = {
+       'message': 'Error internal'
+    }
+    resp = jsonify(message)
+    resp.status_code = 500
+    return resp
+    
+@app.route('/api/v1/votation/vote', methods = ['POST'])
+def vote():
+  try:
+    if request.is_json:
+      data = request.get_json()
+      db = Database(config)
+      db.vote(data['category'])
+      db.close()
+      return (Response(status=200))
+    else:
+      return (Response(status=403))
+    
+  except:
+    message = {
+       'message': 'Error internal'
+    }
+    resp = jsonify(message)
+    resp.status_code = 500
+    return resp
