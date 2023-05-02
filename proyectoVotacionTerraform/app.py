3744e35d70a6504e6610c8914aad1784a904e47b
Paula Erro Alzueta <paulaerroalzueta@MacBook-Air-de-Paula.local>
2023-05-02 11:53:50 +0200

    [16] - subir depliegue parcial de la app a terraform. Sin la base de datos

---
+++ proyectoVotacionTerraform/app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request,Response,jsonify
+import re
+
+app = Flask(__name__)
+
+config = {
+    'user': 'root',
+    'password': 'root',
+    'host': 'mysql',
+    'db': 'voting_data',
+}
+
+def removePunctuationSymbols(text):
+    accepted_chars = re.compile(r'[^0-9a-z]')
+    text = accepted_chars.sub(' ', text).strip()
+    return re.sub(' +', '', text)
+
+@app.route('/', methods=['GET','POST'])
+def votingCatsDogs():
+    try:
+      if request.method == 'POST':
+        #db = Database(config)
+        #if request.form.get('cat') == 'votar':
+        #  db.vote('votation','cat')
+        #elif request.form.get('dog') == 'votar':
+        #  db.vote('votation','dog')
+        cats = 0
+        dogs = 0
+        #db.close()
+        return render_template('voting.html', dog_votes= "Número de votos: " + str(dogs), cat_votes= "Número de votos: " + str(cats))
+      return render_template('voting.html', dog_votes= "", cat_votes= "")
+    
+    except:
+      message = {
+        'message': 'Error internal'
+      }
+      resp = jsonify(message)
+      resp.status_code = 500
+      return resp
+    
